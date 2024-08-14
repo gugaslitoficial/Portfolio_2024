99ac28b1f1696fec0cc129160ae1ce7f0d406760
gugaslitoficial <gustavo-romao@hotmail.com>
2024-08-13 23:26:29 -0300

    Responsividade

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -15,10 +15,10 @@ export default function Home() {
         <div className="h-screen w-full p-8 md:p-16 lg:p-24 xl:p-32 bg-cover bg-center bg-no-repeat"
      style={{ backgroundImage: "url('..//bg-1.jpg')" }}>
 
-    <div className="h-full w-full sm:w-full md:w-1/2 lg:w-full xl:w-full flex flex-col justify-center gap-6">
-        <p className="text-white text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">Hello, I&apos;m</p>
-        <h1 className="text-white text-4xl md:text-5xl lg:text-7xl xl:text-8xl font-bold">Gustavo Romão</h1>
-        <p className="text-white text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold">FULL-STACK DEVELOPER | FREELANCER</p>
+    <div className="h-full w-full sm:w-full md:w-1/2 lg:w-full xl:w-full flex flex-col justify-center gap-6 border-2">
+        <p className="text-white text-3xl sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl font-bold">Hello, I&apos;m</p>
+        <h1 className="text-white text-4xl sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl font-bold">Gustavo Romão</h1>
+        <p className="text-white text-xl sm:text-2xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold">FULL-STACK DEVELOPER | FREELANCER</p>
         <div className="flex flex-wrap items-center gap-3 my-8 md:my-10 xl:my-12">
             <Linkedin
                 size={30}
