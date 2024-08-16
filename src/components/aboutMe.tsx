428cc5f11c6e535875538301b28e6d1ed30afa07
gugaslitoficial <gustavo-romao@hotmail.com>
2024-08-15 23:59:14 -0300

    Responsividade

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -29,14 +29,14 @@ export default function About() {
     }, []);
     
     return (
-        <div className="h-screen w-full p-8 sm:p-4 md:p-8 lg:p-12 xl:p-16 2xl:p-20 bg-cover bg-center bg-no-repeat flex flex-col items-center gap-10 py-20"
+        <div className="h-screen w-full p-1 sm:p-4 md:p-8 lg:p-12 xl:p-16 2xl:p-20 bg-cover bg-center bg-no-repeat flex flex-col items-center gap-3 sm:gap-10 py-20"
         style={{ backgroundImage: "url('bg-2.jpg')" }}>
             <div className="w-full flex flex-col justify-start gap-6">
                 <h1 className=" text-white text-3xl sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl flex items-center justify-center gap-2 font-bold "><CircleUserRound size={iconSize} color="#16a34a" />About Me</h1>
             </div>
             <div className="w-full flex flex-col sm:flex-row">
-                <div className="w-full sm:w-2/4 md:w-2/4 lg:w-3/6 xl:w-1/2 2xl:w-5/12 flex flex-col text-center sm:text-left justify-start items-center gap-3 py-14 sm:py-0 sm:gap-4 md:gap4 lg:gap-6 xl:gap-10 2xl:gap-14 order-2 sm:order-none">
-                    <p className="text-white text-lg sm:text-base md:text-base lg:text-lg xl:text-xl 2xl:text-2xl pt-0 sm:pt-8 md:pt-8 lg:pt-10 xl:pt-20 2xl:pt-36 font-bold ">
+                <div className="w-full sm:w-2/4 md:w-2/4 lg:w-3/6 xl:w-1/2 2xl:w-5/12 flex flex-col text-center sm:text-left justify-start items-center gap-2 sm:py-0 sm:gap-4 md:gap4 lg:gap-6 xl:gap-10 2xl:gap-14 order-2 sm:order-none">
+                    <p className="text-white text-lg sm:text-base md:text-base lg:text-lg xl:text-xl 2xl:text-2xl pt-10 sm:pt-8 md:pt-8 lg:pt-10 xl:pt-20 2xl:pt-36 font-bold ">
                         Hello, I&apos;m Gustavo and I&apos;m a Full Stack Developer with a degree in Systems Analysis and Development and a postgraduate degree in Software Engineering, Network Security and Data Engineering.</p>
                     <p className="text-white text-lg sm:text-base md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-bold ">
                         I really like this big world of programming. Since I was 12 years old, I have been playing with different languages ​​and tools in the field. And as always, creativity is at my side, creating new doors of knowledge and fun.</p>
@@ -47,9 +47,9 @@ export default function About() {
                     alt="Foto de perfil"
                     width={500} 
                     height={500}
-                    className="w-72 sm:w-64 md:w-64 lg:w-72 xl:w-72 2xl:w-96 h-auto object-cover rounded-full border-4 sm:border-2 md-border-2 lg:border-2 xl:border-2 2xl:border-4 border-green-600"
+                    className="w-60 sm:w-64 md:w-64 lg:w-72 xl:w-72 2xl:w-96 h-auto object-cover rounded-full border-4 sm:border-2 md-border-2 lg:border-2 xl:border-2 2xl:border-4 border-green-600"
                     />
-                    <h1 className="text-white text-2xl sm:text-xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl pt-5 font-bold ">Gustavo Romão</h1>
+                    <h1 className="text-white text-2xl sm:text-xl md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl pt-2 sm:pt-5 font-bold ">Gustavo Romão</h1>
                 </div>
 
             </div>
